fb52eaf76777b4c136920a33be6dfaeb226c8514
Greg Noe <gnoe@immregistries.org>
2023-03-26 14:00:09 -0500

    enhance test a bit

--- src/test/java/org/immregistries/mismo/match/PatientMatcherTest.java
+++ src/test/java/org/immregistries/mismo/match/PatientMatcherTest.java
@@ -4,31 +4,84 @@ import static org.junit.Assert.assertEquals;
 import org.immregistries.mismo.match.model.Patient;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class PatientMatcherTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void verifyMatchingWorksByDefault()
-    {
-        PatientMatcher patientMatcher = new PatientMatcher();
-        Patient patientA = new Patient();
-        Patient patientB = new Patient();
-        patientA.setNameFirst("Daniel");
-        patientB.setNameFirst("Daniel");
-        patientA.setNameLast("Smith");
-        patientB.setNameLast("Smith");
-        patientA.setNameMiddle("Henry");
-        patientB.setNameMiddle("Henry");
-        patientA.setBirthDate("20201010");
-        patientB.setBirthDate("20201010");
-        patientA.setMotherMaidenName("Jones");
-        patientB.setMotherMaidenName("Jones");
-        PatientMatchDetermination patientMatchDeterminiation = patientMatcher.match(patientA, patientB);
-        assertEquals(PatientMatchDetermination.MATCH, patientMatchDeterminiation);
+public class PatientMatcherTest {
+
+  @Test
+  public void verifyMatchingWorksByDefault() {
+    long start = System.currentTimeMillis();
+    int count = 1; //100000;
+
+    for (int i = 0; i < count; i++) {
+      PatientMatcher patientMatcher = new PatientMatcher();
+      Patient daniel = new Patient();
+      daniel.setNameFirst("Daniel");
+      daniel.setNameLast("Smith");
+      daniel.setNameMiddle("Henry");
+      daniel.setBirthDate("20201010");
+      daniel.setMotherMaidenName("Jones");
+
+      Patient michael = new Patient();
+      michael.setNameFirst("Michael");
+      michael.setNameLast("Smith");
+      michael.setNameMiddle("Henry");
+      michael.setBirthDate("20201010");
+      michael.setMotherMaidenName("Jones");
+
+      Patient michelle = new Patient();
+      michelle.setNameFirst("Michelle");
+      michelle.setNameLast("Smith");
+      michelle.setNameMiddle("Henry");
+      michelle.setBirthDate("20201010");
+      michelle.setMotherMaidenName("Jones");
+
+      Patient mike = new Patient();
+      mike.setNameFirst("Mike");
+      mike.setNameLast("Smith");
+      mike.setNameMiddle("Henry");
+      mike.setBirthDate("20201010");
+      mike.setMotherMaidenName("Jones");
+
+      Patient daniel2 = new Patient();
+      daniel2.setNameFirst("Daniel");
+      daniel2.setNameLast("Smith");
+      daniel2.setNameMiddle("H");
+      daniel2.setBirthDate("20201010");
+      daniel2.setMotherMaidenName("Jones");
+
+      assertEquals(PatientMatchDetermination.MATCH, patientMatcher.match(daniel, daniel));
+
+      assertEquals(PatientMatchDetermination.MATCH, patientMatcher.match(michael, michael));
+      assertEquals(PatientMatchDetermination.NO_MATCH, patientMatcher.match(daniel, michael));
+
+      assertEquals(PatientMatchDetermination.MATCH, patientMatcher.match(michelle, michelle));
+      assertEquals(PatientMatchDetermination.NO_MATCH, patientMatcher.match(daniel, michelle));
+      assertEquals(PatientMatchDetermination.NO_MATCH, patientMatcher.match(michelle, michael));
+
+      assertEquals(PatientMatchDetermination.MATCH, patientMatcher.match(mike, mike));
+      assertEquals(PatientMatchDetermination.NO_MATCH, patientMatcher.match(daniel, mike));
+      assertEquals(PatientMatchDetermination.NO_MATCH, patientMatcher.match(mike, michelle));
+      assertEquals(PatientMatchDetermination.NO_MATCH, patientMatcher.match(mike, michael));
+
+      assertEquals(PatientMatchDetermination.MATCH, patientMatcher.match(daniel2, daniel2));
+      assertEquals(PatientMatchDetermination.MATCH, patientMatcher.match(daniel, daniel2));
+      assertEquals(PatientMatchDetermination.NO_MATCH, patientMatcher.match(daniel2, mike));
+      assertEquals(PatientMatchDetermination.NO_MATCH, patientMatcher.match(daniel2, michelle));
+      assertEquals(PatientMatchDetermination.NO_MATCH, patientMatcher.match(daniel2, michael));
+
+      if (i % 1000 == 0) {
+        System.out.println(i);
+      }
     }
+
+    int matchesPerLoop = 15; //how many times we call match()
+    int totalCount = count * matchesPerLoop;
+
+    long end = System.currentTimeMillis();
+    long time = end - start;
+    System.out.println("time = " + time + " ms; total count = " + totalCount + "; "
+        + (totalCount * 1.0 / time) + " comparisons per ms");
+
+    // 3/26/2023 on greg's 3.5+ ghz laptop
+    // time = 97493 ms; total count = 1500000; 15.385720000410286 comparisons per ms
+  }
 }

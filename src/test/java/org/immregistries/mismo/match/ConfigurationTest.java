c15562c9f53fa5c5b3e01ba36e12c4b32b5f8339
Nathan Bunker <nbunker@immregistries.org>
2025-04-07 12:13:53 -0600

    Added test to make sure configuration is being read properly

--- src/test/java/org/immregistries/mismo/match/ConfigurationTest.java
+++ src/test/java/org/immregistries/mismo/match/ConfigurationTest.java
@@ -1,6 +1,9 @@
 package org.immregistries.mismo.match;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStream;
 
 import org.immregistries.mismo.match.model.Configuration;
 import org.junit.Test;
@@ -15,6 +18,22 @@ public class ConfigurationTest {
             Configuration configuration = new Configuration();
             configuration.setup();
             assertTrue(configuration != null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testOriginal() {
+        try {
+            InputStream inputStream = getClass().getResourceAsStream("/mismo-matcher.original.yml");
+            Configuration configuration = new Configuration(inputStream);
+            configuration.setup();
+            assertTrue(configuration != null);
+            assertEquals("Boston19", configuration.getIslandName());
+            assertEquals(0.5976539440304252, configuration.getMatch().getMaxScore(), 0.0001);
+            assertEquals("Household", configuration.getMatch().getMatchNodeList().get(0).getMatchName());
+            assertEquals(0.5340047021220341, configuration.getMatch().getMatchNodeList().get(0).getMaxScore(), 0.0001);
         } catch (Exception e) {
             e.printStackTrace();
         }
